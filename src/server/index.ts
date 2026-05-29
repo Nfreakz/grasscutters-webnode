9282d9ac92b941b3431022172068557e2c1815a8
NFreakz <dahemo@gmail.com>
2026-05-29 12:02:20 +0200

    New ui update 0.0.1

--- src/server/index.ts
+++ src/server/index.ts
@@ -12476,6 +12476,11 @@ app.get('/api/logout', (req, res) => {
 
 
 
+
+
+
+
+
 /* GC_ASTRO_RUNTIME_PATCH_V3
  * Runtime Hostinger + Astro para Express.
  * V3: separa API, estáticos prerenderizados y SSR.
